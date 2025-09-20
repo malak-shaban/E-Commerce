5d3976ed22bfead6b94bac86a72fd15bc6ad4ff8
Malak Shaban <m14632423@gmail.com>
2025-09-20 07:27:39 -0700

    message

--- src/app/_Component/CategorySlider/CategorySwiper/CategorySwiper.tsx
+++ src/app/_Component/CategorySlider/CategorySwiper/CategorySwiper.tsx
@@ -6,30 +6,36 @@ import { Autoplay } from 'swiper/modules'
 import Image from 'next/image';
 import { categoryType } from '../../../../types/category.type';
 
-export default function CategorySwiper({data} : {data : categoryType[]}) {
-      console.log(data);
-    return (
-      <div className='w-[80%] mx-auto my-4 font-serif p-2'>
-         <h2 className='my-3 text-2xl text-gray-700'>Shop Popular Categories</h2>
+export default function CategorySwiper({ data }: { data: categoryType[] }) {
+  console.log(data);
+  return (
+    <div className='w-[80%] mx-auto my-4 font-serif p-2'>
+      <h2 className='my-3 text-2xl text-gray-700'>Shop Popular Categories</h2>
       <div className="flex">
-            <Swiper
-            spaceBetween={0}
-            slidesPerView={7}
-            modules={[Autoplay]}
-            autoplay={{ delay: 2000 }}
 
+        <Swiper
+          spaceBetween={0}
+          slidesPerView={2}
+          modules={[Autoplay]}
+          autoplay={{ delay: 2000 }}
+          breakpoints={{
+            640: { slidesPerView: 3 },
+            768: { slidesPerView: 4 },
+            1024: { slidesPerView: 5 },
+            1280: { slidesPerView: 7 }
+          }}
         >
-          {data.map((category:categoryType)=>(
-              <SwiperSlide key={category._id}>
-                 <Image src={category.image} alt='photo' width={500} height={500} className='w-full h-[150px] object-cover' />
-                 <p className='font-bold pt-2'>{category.name}</p>
-              </SwiperSlide>
+          {data.map((category: categoryType) => (
+            <SwiperSlide key={category._id}>
+              <Image src={category.image} alt='photo' width={500} height={500} className='w-full h-[150px] object-cover' />
+              <p className='font-bold pt-2'>{category.name}</p>
+            </SwiperSlide>
           ))}
-            
+
         </Swiper>
       </div>
-      </div>
+    </div>
 
-       
-    )
+
+  )
 }
--- src/app/_Component/MainSlider/MainSlider.tsx
+++ src/app/_Component/MainSlider/MainSlider.tsx
@@ -8,32 +8,61 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css/bundle';
 import { Autoplay } from 'swiper/modules'
 
-
 export default function MainSlider() {
   return (
-    <>
-    <div className='w-[80%] mx-auto my-4 flex'>
-      <div className="w-3/4">
-       <Swiper
-      spaceBetween={0}
-      slidesPerView={1}
-      modules={[Autoplay]}
-      autoplay={{delay:2000}}
-     
-    >
-      <SwiperSlide> <Image src={img1} alt='photo' className='w-full h-[400px] object-cover' loading="eager" priority/></SwiperSlide>
-      <SwiperSlide> <Image src={img2} alt='photo' className='w-full h-[400px] object-cover' loading="eager" priority /></SwiperSlide>
-      <SwiperSlide> <Image src={img3} alt='photo' className='w-full h-[400px] object-cover' loading="eager" priority /></SwiperSlide>
-    </Swiper>
+    <div className='w-[80%] mx-auto my-4 flex flex-col lg:flex-row'>
+      <div className="w-full lg:w-3/4">
+        <Swiper
+          spaceBetween={0}
+          slidesPerView={1}
+          modules={[Autoplay]}
+          autoplay={{ delay: 2000 }}
+        >
+          <SwiperSlide>
+            <Image
+              src={img1}
+              alt='photo'
+              className='w-full h-[400px] object-cover'
+              loading="eager"
+              priority
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image
+              src={img2}
+              alt='photo'
+              className='w-full h-[400px] object-cover'
+              loading="eager"
+              priority
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image
+              src={img3}
+              alt='photo'
+              className='w-full h-[400px] object-cover'
+              loading="eager"
+              priority
+            />
+          </SwiperSlide>
+        </Swiper>
       </div>
-      <div className="w-1/4 flex flex-col">
-      <Image src={img2} alt='photo' className='w-full h-[200px] object-cover' loading="eager" priority />
-      <Image src={img3} alt='photo' className='w-full h-[200px] object-cover' loading="eager" priority />
+      <div className="w-full lg:w-1/4 flex flex-col">
+        <Image
+          src={img2}
+          alt='photo'
+          className='w-full h-[200px] object-cover'
+          loading="eager"
+          priority
+        />
+        <Image
+          src={img3}
+          alt='photo'
+          className='w-full h-[200px] object-cover'
+          loading="eager"
+          priority
+        />
       </div>
     </div>
-
-
-
-    </>
   )
 }
