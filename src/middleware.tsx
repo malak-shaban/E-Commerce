27ed5fc711e63d86c7443d6e86e438afd24be535
Malak Shaban <m14632423@gmail.com>
2025-09-21 08:07:02 -0700

    fix: update middleware logic

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,27 +1,50 @@
-import { getToken } from 'next-auth/jwt'
-import { NextRequest, NextResponse } from 'next/server'
+// import { getToken } from 'next-auth/jwt'
+// import { NextRequest, NextResponse } from 'next/server'
+// export async function middleware(request : NextRequest) {
+//   const token = await getToken({req : request})
+//   if(token){
+//     if(request.nextUrl.pathname === "/login" || request.nextUrl.pathname === "/register" ){
+//       return NextResponse.redirect(new URL('/' , request.url ))
+//     }
+//     else{
+//       return NextResponse.next() //go to place you want
+//     }
+//   }
+//   else{
+//     if(request.nextUrl.pathname === "/cart"){
+//       return NextResponse.redirect(new URL('/login' , request.url ))
+//     }
+//    else{
+//      return NextResponse.next() //go to place you want
+//    }
+// }
+// }
+// export const config = {
+//     matcher: ['/cart', '/login' , '/register' ]
+// }
 
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function middleware(request : NextRequest) {
-  const token = await getToken({req : request})
-  if(token){
-    if(request.nextUrl.pathname === "/login" || request.nextUrl.pathname === "/register" ){
-      return NextResponse.redirect(new URL('/' , request.url ))
+export async function middleware(request: NextRequest) {
+  const token = await getToken({ req: request });
+  if (token) {
+    if (request.nextUrl.pathname === "/login" || request.nextUrl.pathname === "/register") {
+      return NextResponse.redirect(new URL("/", request.url));
     }
-    else{
-      return NextResponse.next() //go to place you want
-    }
+    return NextResponse.next();
   }
-  else{
-    if(request.nextUrl.pathname === "/cart"){
-      return NextResponse.redirect(new URL('/login' , request.url ))
+  else {
+    const protectedRoutes = ["/cart", "/orders", "/users", "/wishlist"];
+
+    if (protectedRoutes.includes(request.nextUrl.pathname)) {
+      return NextResponse.redirect(new URL("/login", request.url));
     }
-   else{
-     return NextResponse.next() //go to place you want
-   }
-}
-}
-export const config = {
-    matcher: ['/cart', '/login' , '/register' ]
+
+    return NextResponse.next();
+  }
 }
 
+export const config = {
+  matcher: ["/cart", "/orders", "/users", "/wishlist", "/login", "/register"],
+};
